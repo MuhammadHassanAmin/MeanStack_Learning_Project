e318c94c466410489ec012de93cdbc56a81f61b6
Hassan Amin <hasan_amin3@yahoo.com>
2019-07-10 15:20:41 +0500

    adding material design

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {CreatePostComponent} from './posts/create-post-component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatInputModule} from '@angular/material'
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreatePostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
